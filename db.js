cffc26e133e30b633b74b7d2e86ff01826fd2508
Abhijeet <pawar2467@gmail.com>
2019-12-02 15:27:33 +0530

    added new ip

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 function connect()
 {
     const connection = mysql.createConnection({
-        host: '192.168.0.112',
+        host: '172.18.4.90',
         user: 'root',
         password: 'root',
         database: 'myapp_db',
